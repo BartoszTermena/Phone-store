28aea45edc7110d231bef64063121d97755faad1
bartek <Bartek.NNAAN@wp.pl>
2019-01-27 10:06:07 +0100

    propTypes, context

---
+++ src/context.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react'
+import {storeProducts, detailProduct} from './data';
+
+const ProductContext = React.createContext();
+
+class ProductProvider extends Component {
+  state = {
+    products: storeProducts,
+    detailProduct: detailProduct
+  }
+  handleDetail = () => {
+    console.log('hello from detail');
+  }
+  addToCart = () => {
+    console.log("Hello from cart")
+  }
+  tester = () => {
+    console.log('state products: ', this.state.products[0].inCart);
+    console.log('Data products: ', storeProducts[0].inCart)
+  
+    
+  }
+  render() {
+    return (
+      <ProductContext.Provider 
+      value={{
+        ...this.state,
+        handleDetail: this.handleDetail,
+        addToCart: this.addToCart
+      }}>
+        {this.props.children}
+      </ProductContext.Provider>
+    )
+  }
+}
+const ProductConsumer = ProductContext.Consumer;
+
+export { ProductProvider, ProductConsumer };
